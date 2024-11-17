b81ac2386f2a591ca2d939b6eb71915b835088e5
YuLuowo <diego050415@gmail.com>
2024-11-17 17:00:25 +0800

    Create App.vue

---
+++ app/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="app-container">
+        <NavigationBar/>
+        <router-view />
+    </div>
+</template>
+
+<script>
+import NavigationBar from "../components/NavigationBar.vue";
+import {useSeoMeta} from '@vueuse/head'
+
+export default {
+    name: "HomePage",
+    components: {
+        NavigationBar,
+    },
+    setup() {
+        useSeoMeta({
+            title: 'ImagineYuLuo'
+        })
+    }
+};
+
+</script>
+
+<style>
+.app-container {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+}
+
+</style>
