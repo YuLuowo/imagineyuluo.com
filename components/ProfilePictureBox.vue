5e4cdaf3402c25201c4dc799975fa0c8e358db3a
YuLuo <diego050415@gmail.com>
2024-11-22 08:00:28 +0800

    Create ProfilePictureBox.vue

---
+++ components/ProfilePictureBox.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+<div class="picture-box">
+
+</div>
+</template>
+
+<style scoped>
+.picture-box {
+    width: 150px;
+    height: 150px;
+    background: url("../public/images/profile_picture.jpg");
+    background-size: cover;
+    border-radius: 50%;
+    display: inline-block;
+    border: 1px solid #eeeeee;
+}
+</style>
