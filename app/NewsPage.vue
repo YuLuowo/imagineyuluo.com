befa735c71e790021677bf5f98e505c88a65bf4d
YuLuowo <diego050415@gmail.com>
2024-12-05 16:45:46 +0800

    Update NewsPage.vue

--- app/NewsPage.vue
+++ app/NewsPage.vue
@@ -1,16 +1,23 @@
 <template>
-    <h1>News Page</h1>
+    <h1>News</h1>
     <div class="news-container">
-        <div class="group-box"></div>
+        <div class="group-box">
+            <SearchNews/>
+            <NewsGroup />
+        </div>
     </div>
 </template>
 
 <script>
 import {useSeoMeta} from '@vueuse/head'
+import NewsGroup from "../components/NewsGroup.vue";
+import SearchNews from "../components/SearchNews.vue";
 
 export default {
     name: "NewsPage",
     components: {
+        NewsGroup,
+        SearchNews,
     },
     setup() {
         useSeoMeta({
@@ -30,13 +37,20 @@ export default {
 }
 
 .group-box {
-    width: 70%;
-    height: 540px;
+    width: 40%;
     background-color: rgba(255, 255, 255, 0.9);
     border: 1px solid rgba(209, 217, 224, 0.7);
     border-radius: 5px;
     box-shadow: rgba(37, 41, 46, 0.12) 0px 3px 6px 0px;
-    padding: 50px;
+    padding: 20px;
     margin: 30px;
+}
+
+@media screen and (max-width: 768px) {
+    .group-box {
+        width: 100%;
+        padding: 20px;
+        margin: 20px;
+    }
 }
 </style>
