bb29d9acf4367875bb7f77a2567c26b7f76bedb9
YuLuowo <diego050415@gmail.com>
2024-12-05 16:45:49 +0800

    Create SearchNews.vue

---
+++ components/SearchNews.vue
@@ -0,0 +1,64 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="search-news">
+    <input type="text" id="search-input" placeholder="" />
+    <button><i class="fas fa-search"></i> Search</button>
+</div>
+</template>
+
+<style scoped>
+    .search-news {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 10px;
+        gap: 5px;
+    }
+
+    .search-news input {
+        width: 70%;
+        font-size: 16px;
+        padding: 5px;
+    }
+
+    .search-news button {
+        width: 30%;
+        font-size: 16px;
+        background: black;
+        color: white;
+        border: none;
+    }
+
+    .search-news button:hover {
+        cursor: pointer;
+        background: rgba(0, 0, 0, 0.8);
+    }
+
+    @media screen and (max-width: 768px) {
+        .search-news {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-bottom: 10px;
+            gap: 5px;
+        }
+
+        .search-news input {
+            width: 90%;
+            font-size: 20px;
+        }
+
+        .search-news button {
+            width: 30%;
+            font-size: 16px;
+            background: black;
+            color: white;
+            border: none;
+            padding: 5px;
+        }
+    }
+
+
+</style>
