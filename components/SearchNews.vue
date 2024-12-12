bf884026d4195ac0dfd26eccbf7ebe8dd84b45d3
YuLuowo <diego050415@gmail.com>
2024-12-12 16:52:42 +0800

    Update SearchNews.vue

--- components/SearchNews.vue
+++ components/SearchNews.vue
@@ -1,64 +1,55 @@
 <script setup>
+import {defineProps, defineEmits} from 'vue';
 
+defineProps(['modelValue']);
+const emit = defineEmits(['update:modelValue']);
+
+function updateSearch(e) {
+    emit('update:modelValue', e.target.value);
+}
 </script>
 
 <template>
-<div class="search-news">
-    <input type="text" id="search-input" placeholder="" />
-    <button><i class="fas fa-search"></i> Search</button>
-</div>
+    <div class="search-news">
+        <i class="fa fa-search"></i>
+        <input
+            type="text"
+            id="search-input"
+            placeholder="Search the news..."
+            :value="modelValue"
+            @input="updateSearch"
+        />
+    </div>
 </template>
 
 <style scoped>
     .search-news {
         display: flex;
-        justify-content: space-between;
-        margin-bottom: 10px;
-        gap: 5px;
+        align-items: center;
+        width: 100%;
+        background-color: rgb(255, 255, 255);
+        border: 1px solid rgba(209, 217, 224, 0.7);
+        border-radius: 3px;
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+        padding: 10px 0 10px 0;
+    }
+
+    .search-news i {
+        font-size: 16px;
+        color: #888888;
+        margin: 0 10px;
     }
 
     .search-news input {
-        width: 70%;
-        font-size: 16px;
-        padding: 5px;
-    }
-
-    .search-news button {
-        width: 30%;
-        font-size: 16px;
-        background: black;
-        color: white;
         border: none;
+        outline: none;
+        width: 100%;
+        font-size: 14px;
+        color: #333333;
+        background-color: transparent;
     }
 
-    .search-news button:hover {
-        cursor: pointer;
-        background: rgba(0, 0, 0, 0.8);
+    .search-news input::placeholder {
+        color: #bbbbbb;
     }
-
-    @media screen and (max-width: 768px) {
-        .search-news {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            margin-bottom: 10px;
-            gap: 5px;
-        }
-
-        .search-news input {
-            width: 90%;
-            font-size: 20px;
-        }
-
-        .search-news button {
-            width: 30%;
-            font-size: 16px;
-            background: black;
-            color: white;
-            border: none;
-            padding: 5px;
-        }
-    }
-
-
 </style>
